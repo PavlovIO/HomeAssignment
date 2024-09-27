08911fc09010fb8bb1e9f2f590c3a38343d65a8b
PavlovIO <ilya-pavlov-02.08@mail.ru>
2024-09-27 12:13:30 +0300

    Create directory Problem-1

---
+++ Problem-1/sayhello.cpp
@@ -0,0 +1,13 @@
+/* Ilya Pavlov st129535@student.spbu.ru
+	first test programm
+*/
+
+#include <iostream>
+#include <string>
+#include "hello.h"
+
+void sh::SayHello(std::string name)
+{
+	std::cout<<"Hello, " + name + " !!!"<<std::endl;
+	
+}
