cb685de2fa9d9005c771383839fc165d012063e8
PavlovIO <ilya-pavlov-02.08@mail.ru>
2024-10-09 23:55:42 +0300

    moved files from work to src dir

---
+++ Assignment2/b/src/main.cpp
@@ -0,0 +1,63 @@
+#include "header.h"
+int main(){
+	std::string ps;
+	std::cout << "Input your equation:";
+	std::getline(std::cin, ps);
+
+	std::cout<<ps<<std::endl;
+	
+	int len = 0;
+	std::string* ps_f;
+	ps_f = str_form(ps, len);
+	
+	double* stack = new double[len];
+	int k = 0;
+
+	for (int i = 0; i<len; i++){
+	    if (isDouble(ps_f[i])){
+		stack[k] = std::stod(ps_f[i]);
+		k++;
+	    }else{
+	        if (k<2){
+	            std::cout << "Not enough operands";
+                    delete[] stack;
+                    return 0;
+	        }
+	        char op = ps_f[i][0];
+		switch(op){
+		    case '+':
+		        stack[k-2] = stack[k-2]+stack[k-1];
+		        k--;
+		        break;
+		    case '-':
+		        stack[k-2] = stack[k-2]-stack[k-1];
+		        k--;
+		        break;
+		    case '*':
+		        stack[k-2] = stack[k-2]*stack[k-1];
+		        k--;
+		        break;
+		    case '/':
+		        if (stack[k-1] == 0){
+		            std::cout << "Division by 0";
+		            delete[] stack;
+		            delete[] ps_f;
+		            return 0;
+		        }
+		        stack[k-2] = stack[k-2]/stack[k-1];
+		        k--;
+		        break;
+		    default:
+		        std::cout << "Wrong operator";
+		        delete[] stack;
+		        delete[] ps_f;
+		        return 0;
+		}
+	    }
+	    
+	}
+	std::cout << "Result: "<< stack[0]<<std::endl;
+	delete[] stack;
+	delete[] ps_f;
+	return 0;
+}
