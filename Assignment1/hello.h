8716482d1612150e62b89e7f1b04fae636a628a1
PavlovIO <ilya-pavlov-02.08@mail.ru>
2024-10-18 11:50:29 +0300

    Renamed into Assignment1

---
+++ Assignment1/hello.h
@@ -0,0 +1,15 @@
+/* Ilya Pavlov st129535@student.spbu.ru
+	first test programm
+*/
+
+#ifndef HELLO_H
+#define HELLO_H
+#include <string>
+
+namespace sh
+{
+	void SayHello(std::string name);
+	void Cycle();
+}
+#endif
+
